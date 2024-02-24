721ecc63012ba3fa4cde095b4fe3cfb06f375d54
Saad <Saad@DESKTOP-1K9C1MF>
2024-02-24 16:37:06 +0500

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BLL.IRepositries;
+using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mybookstore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepo _context;
+        public CategoryController( ICategoryRepo context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+            return Ok(_context.GetAll());
+            
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+using BLL.IRepositries;
+using DAL.Data;
+using BLL.Repositries;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<ICategoryRepo,CategoryRepo>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
